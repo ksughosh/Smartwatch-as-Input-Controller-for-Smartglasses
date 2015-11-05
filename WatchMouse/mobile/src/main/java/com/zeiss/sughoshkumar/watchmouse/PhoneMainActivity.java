5d1e44160832fd5b1a1748620e671a953e083b76
Sughosh Krishna Kumar <zoskris@zolxq00112.deoko.zeiss.org>
2015-11-05 10:51:52 +0100

    Updated the Client code

--- WatchMouse/mobile/src/main/java/com/zeiss/sughoshkumar/watchmouse/PhoneMainActivity.java
+++ WatchMouse/mobile/src/main/java/com/zeiss/sughoshkumar/watchmouse/PhoneMainActivity.java
@@ -1,9 +1,12 @@
 package com.zeiss.sughoshkumar.watchmouse;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.zeiss.sughoshkumar.senderobject.SenderObject;
 
